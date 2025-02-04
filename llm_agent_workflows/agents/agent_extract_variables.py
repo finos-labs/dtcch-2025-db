d7d7afffbdf346970534a93396dc360b2477530a
alesordo <ale.sordo@hotmail.it>
2025-02-04 20:02:52 +0100

    Add variables_extractor
    Signed-off-by: alesordo <ale.sordo@hotmail.it>

---
+++ llm_agent_workflows/agents/agent_extract_variables.py
@@ -0,0 +1,50 @@
+import json
+
+from typing import List, Dict, Union
+
+from crew import Agent
+
+class AgentExtractVariables(Agent):
+    def __init__(self, bedrock_client=None):
+        """Initialize the variables extractor agent with AWS Bedrock client."""
+        super().__init__("", "", "")
+        self.bedrock_client = bedrock_client or self._init_bedrock_client()
+
+    def _analyze_page(self, action, quote, variables: Dict[str, str]) -> Dict[str, str]:
+        """Extract variables using AWS Bedrock."""
+        # Prepare the prompt
+        prompt = f"""Analyze the following action (aka KYC requirement) and quote from a policy document, and provide:
+        1. The variable role of the party the action relates to. Select only one value from {variables['role']}.
+        2. The variable due diligence level that the action relates to. Select one or more values from {variables['due_diligence']}
+        3. The variable type of business being conducted with the client. Select only one value from {variables['business_type']}
+        4. The variable entity type the action relates to. Select only one value from {variables['entity_type']}.
+        5. The variable internal evidence. Select one or more values from {variables['internal_evidence']}
+        6. The variable public evidence. Select one or more values from {variables['public_evidence']}
+        5. The variable client evidence. Select one or more values from {variables['non_public_evidence']}
+
+        
+        Action: {action}
+        Quote from which the action was extracted: {quote}
+        
+        Additional Context: you should only return a value for each variable from the provided list of available ones.
+        
+        Please format your response as JSON with the following structure:
+        {{
+            "role": "specific role of the action's party",
+            "due_diligence_level": ["due_diligence_level1", "due_diligence_level2", ...]
+            "business_type": "specific type of business conducted with the client",
+            "entity_type": "specific type of entity the action relates to",
+            "internal_evidence": ["evidence1", "evidence2", ...],
+            "public_evidence": ["evidence1", "evidence2", ...],
+            "client_evidence": ["evidence1", "evidence2", ...]
+        }}
+        """
+        
+        response = self.invoke_bedrock(prompt)
+        try:
+            if not response:
+                return {"labels": [], "summary": "Error analyzing action"}
+            return json.loads(response)
+        except json.JSONDecodeError:
+            print(f"Error parsing response for action {action}")
+            return {"labels": [], "summary": "Error analyzing action"}
