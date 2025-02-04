bccd539af399a7792d07b6ec79ea18219c354146
valebladi <valebladi@gmail.com>
2025-02-04 17:41:10 +0100

    added functionality of sections to actions and data points
    Signed-off-by: valebladi <valebladi@gmail.com>

--- llm_agent_workflows/agents/agent_sections_to_actions.py
+++ llm_agent_workflows/agents/agent_sections_to_actions.py
@@ -2,7 +2,7 @@ import json
 
 from typing import List, Dict, Union
 
-from crew import Agent
+from crew import Agent, Task
 
 
 class AgentSectionsToActions(Agent):
@@ -11,9 +11,9 @@ class AgentSectionsToActions(Agent):
         """Initialize the PDF Handler with AWS Bedrock client."""
         super().__init__()
         # Set required attributes
-        self.role = "assistant"
-        self.goal = "Process document sections into actions"
-        self.backstory = "An AI agent designed to extract structured actions from documents."
+        self.role = "KYC Compliance Officer"
+        self.goal = "Identify KYC-relevant sections from a given policy, map each action to the required data points, Format the output into a structured JSON format."
+        self.backstory = "With over 10 years of experience in financial compliance, the KYC Compliance Officer has worked with global financial institutions, helping them align with international AML/KYC regulations like FATF, FINCEN, and GDPR. They use a structured approach to ensure data accuracy and compliance integrity."
 
         self.bedrock_client = bedrock_client or self._init_bedrock_client()
 
@@ -44,3 +44,18 @@ class AgentSectionsToActions(Agent):
         except json.JSONDecodeError:
             print(f"Error parsing response for page {page_num}")
             return {"labels": [], "summary": "Error analyzing page"}
+
+    def task_section_to_actions(self) -> Task:
+        """Task: Convert document sections into actionable items."""
+        section = " Where a firm advises the owners of securities, in respect of the repurchase, exchange or redemption by an issuer of those securities, the owners will be customers of the firm for AML purposes."
+        # things to do
+        # 1. separate the policy section into individual
+        # 2. make sure the policy has actionable items
+        # 3. add data points to the actionable items
+
+        return Task(
+            description=f"The primary objective of this task is to process a given KYC policy document section. ({section}) and extract KYC-related actions while identifying the corresponding data points associated with each action. This ensures compliance with regulatory frameworks such as FATF, FINCEN, GDPR, and AML directives. The process follows, to identifying key KYC actions such as verification, monitoring, reporting, and record-keeping, and map the extracted actions to specific data points (e.g., Name, ID, Source of Funds).",
+            agent_role="KYC Compliance Officer",
+            expected_output="Matrix format showing the section, action, and data point.",
+            dependencies=[]
+        )
