5342981785ee72b3d7f54d44e5e70d10257b0c4d
pailwan <sachinpailwan@gmail.com>
2025-02-05 17:07:57 +0100

    dashboard-integration
    Signed-off-by: pailwan <sachinpailwan@gmail.com>

--- kyc-ui-dashboard/src/config.js
+++ kyc-ui-dashboard/src/config.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "http://localhost:5000"; // Change this to your actual API URL
+// const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "http://dtcch-2025-db.sibnick.men:5000/"; // Change this to your actual API URL
 
 export const API_ENDPOINTS = {
   BASE_URL: `${API_BASE_URL}/`,    
