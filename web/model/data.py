a3a9849a9c9bfadafe1d7cc7eee612589dbdffa5
yonka <yonkaps@gmail.com>
2025-02-05 11:40:43 +0100

    flask api
    Signed-off-by: yonka <yonkaps@gmail.com>

---
+++ web/model/data.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+@dataclass
+class Kyc:
+    kycId: int
+    clientId: int
+    clientName: str
+    date: datetime
+    status: str
+    afcStatus: str = 'none'
+    riskRate: str = 'none'
+
+@dataclass
+class Client:
+    clientId: int
+    clientName: str
+    clientEmail: str
+
+@dataclass
+class Ops:
+    opsId: int
+    opsName: str
+    opsDetails: str
