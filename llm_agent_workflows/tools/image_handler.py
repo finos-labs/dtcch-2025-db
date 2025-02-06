fc4f9f580375f1b8af150b82c14887bfedd2f23d
valebladi <valebladi@gmail.com>
2025-02-06 10:16:59 +0100

    added risk agent and relevant examples of risk documentation
    Signed-off-by: valebladi <valebladi@gmail.com>

---
+++ llm_agent_workflows/tools/image_handler.py
@@ -0,0 +1,19 @@
+import pytesseract
+from PIL import Image
+
+# Set the tesseract path
+pytesseract.pytesseract.tesseract_cmd = "/usr/local/bin/tesseract"
+
+# Open the image using Pillow
+image = Image.open("/llm_agent_workflows/examples_evidence/evidence_driving_licence.jpeg")
+
+# Pass the image to pytesseract for text extraction
+text = pytesseract.image_to_string(image)
+
+
+print("Extracted Text:", text)
+
+# 0. send information to llm to format, not exactly know how
+# 1. join information from internal DB and 0. to have the complete profile of the client
+# 2. send this information to the KYC risk agent to check if the profile is risky, with the risk csv
+
