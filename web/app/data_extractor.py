5058729edb39078816671fdd920d584502d7ecf8
sibnick <sibnick@gmail.com>
2025-02-06 14:23:41 +0100

    revert Changes
    Signed-off-by: sibnick <sibnick@gmail.com>

--- web/app/data_extractor.py
+++
@@ -1,9 +0,0 @@
-import multiprocessing
-import time
-
-def worker_function(uid, files):
-    print("Processing files for", uid, files)
-
-def process_files(uid, files):
-    process = multiprocessing.Process(target=worker_function, args=(uid, files))
-    process.start()
