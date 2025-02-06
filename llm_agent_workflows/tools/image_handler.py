4333c70f1a95941de8f110aacb2905e202f48184
valebladi <valebladi@gmail.com>
2025-02-06 11:42:54 +0100

    added steps for the risk
    Signed-off-by: valebladi <valebladi@gmail.com>

--- llm_agent_workflows/tools/image_handler.py
+++ llm_agent_workflows/tools/image_handler.py
@@ -10,25 +10,33 @@ class RiskHandler:
         """Initialize the JSON Handler with AWS Bedrock client."""
         self.agent = AgentRiskAssessment()
 
-# Set the tesseract path
-pytesseract.pytesseract.tesseract_cmd = "/usr/local/bin/tesseract"
+    def extract_text_ocr(self, image_path: str) -> str:
+        """Extract text from an image using Tesseract OCR."""
 
-# Open the image using Pillow
-image = Image.open("/llm_agent_workflows/examples_evidence/evidence_driving_licence.jpeg")
+        # Set the tesseract path
+        pytesseract.pytesseract.tesseract_cmd = "/usr/local/bin/tesseract"
+        # Open the image using Pillow
+        image = Image.open(image_path)
+        # Pass the image to pytesseract for text extraction
+        text = pytesseract.image_to_string(image)
+        print("Extracted Text:", text)
+        return text
 
-# Pass the image to pytesseract for text extraction
-text = pytesseract.image_to_string(image)
+    def data_clean_and_insert_in_db(self, text: str) -> str:
+        """Clean and format extracted text and insert into the database."""
+        # Clean and format the extracted text
+        cleaned_text = self.agent._evidence_clean(text)
+        # TODO: Insert the cleaned text into the database
+        # insert clean evidence in evidence type into actions
 
+    def risk_assessment(self, risks: list, complete_profile: list) -> str:
+        """Process extracted text evidence and perform risk assessment."""
 
-print("Extracted Text:", text)
+        # TODO: read csv under risk/risks.csv
 
+        # Perform risk assessment based on risks and complete profile
+        risk_client = self.agent._risk_assessment(risks, complete_profile)
 
-# 0. send information to llm to format,
-#   (evidence_clean)
-# 1. join information from internal DB and 0. to have the complete profile of the client insert to db
-#   insert clean evidence in evidence type into actions
-# 2. send this information to the KYC risk agent to check if the profile is risky, with the risk csv
-#   fetch all actions (as user profile is complete)
-# 2.1 read the db with inserted evidence and send it to the risk agent
-#   read csv under risk/risks.csv and send it to risk assesmne
-# 2.2 once we receive the risk assessment insert it into db,
+        # TODO: add validation of the json (alessio)
+
+        # TODO: insert into data base
