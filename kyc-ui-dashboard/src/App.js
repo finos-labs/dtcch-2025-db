53dc702f52429a3ceb06889e9f7647f0e46df89a
pailwan <sachinpailwan@gmail.com>
2025-02-06 08:41:51 +0100

    upload documents
    Signed-off-by: pailwan <sachinpailwan@gmail.com>

--- kyc-ui-dashboard/src/App.js
+++ kyc-ui-dashboard/src/App.js
@@ -1,15 +1,22 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Navigate  } from 'react-router-dom';
-import Dashboard from './components/Dashboard';
-import KycDetails from './components/KycDetails';
-import LoginPage from './components/Login';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import KycDetails from "./components/KycDetails";
+import LoginPage from "./components/Login";
+import DocumentUpload from "./components/DocumentUpload";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/kyc/:kycId" element={<KycDetails /> } />
+        <Route path="/kyc/:kycId" element={<KycDetails />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/upload" element={<DocumentUpload />} />
         <Route path="/" element={<LoginPage />} />
       </Routes>
     </Router>
--- kyc-ui-dashboard/src/config.js
+++ kyc-ui-dashboard/src/config.js
@@ -1,11 +1,5 @@
-// const API_BASE_URL = "http://localhost:5000";
 const API_BASE_URL = "http://dtcch-2025-db.sibnick.men:5000/"; // Change this to your actual API URL
 
 export const API_ENDPOINTS = {
-  BASE_URL: `${API_BASE_URL}/`,    
-  GET_DATA: `${API_BASE_URL}/data`,
-  POST_DATA: `${API_BASE_URL}/submit`,
-  AUTH_LOGIN: `${API_BASE_URL}/auth/login`,
-  AUTH_REGISTER: `${API_BASE_URL}/auth/register`,
-  USER_DATA: `${API_BASE_URL}/user`,
+  BASE_URL: `${API_BASE_URL}/`,
 };
