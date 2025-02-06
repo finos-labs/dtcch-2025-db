ceefbad2ae677b19840dc19c584ce8a59377ba39
valebladi <valebladi@gmail.com>
2025-02-06 11:02:25 +0100

    added steps
    Signed-off-by: valebladi <valebladi@gmail.com>

--- llm_agent_workflows/tools/image_handler.py
+++ llm_agent_workflows/tools/image_handler.py
@@ -14,6 +14,7 @@ text = pytesseract.image_to_string(image)
 print("Extracted Text:", text)
 
 # 0. send information to llm to format, not exactly know how
-# 1. join information from internal DB and 0. to have the complete profile of the client
+# 1. join information from internal DB and 0. to have the complete profile of the client insert to db
 # 2. send this information to the KYC risk agent to check if the profile is risky, with the risk csv
-
+# 2.1 read the db with inserted evidence and send it to the risk agent
+# 2.2 once we recive the risk assessment insert it into db,
