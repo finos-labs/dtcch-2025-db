d7619c2eb6b4f96d8c1921a2df4d0df04076d978
valebladi <valebladi@gmail.com>
2025-02-06 16:52:25 +0100

    fixed imports
    Signed-off-by: valebladi <valebladi@gmail.com>

--- llm_agent_workflows/main.py
+++ llm_agent_workflows/main.py
@@ -14,8 +14,8 @@ import fitz
 from pathlib import Path
 import os
 import time
-from .tools.evidence_handler import EvidenceHandler
-from .tools.risk_handler import RiskHandler
+from tools.evidence_handler import EvidenceHandler
+from tools.risk_handler import RiskHandler
 
 class SectionOutput(BaseModel):
     model_config = ConfigDict(strict=True)
@@ -25,14 +25,14 @@ class SectionOutput(BaseModel):
     data_point: str
 
 def main():
-    RISK_PATH = "llm_agent_workflows/tools/input/risks/risks.csv"
+    RISK_PATH = "tools/input/risks/risks.csv"
     evidence_handler = EvidenceHandler()
     risk_handler = RiskHandler()
 
     # TODO: this will be passed when this is triggered
     kyc_id = 2
     data_point = "Transaction Monitoring"
-    image_path = "llm_agent_workflows/tools/input/examples_evidence/evidence_driving_licence.jpeg"
+    image_path = "tools/input/examples_evidence/evidence_driving_licence.jpeg"
 
     # the evidence will process the image and extract the text and insert it in the db
     evidence_handler.process_evidence(image_path, kyc_id, data_point)
